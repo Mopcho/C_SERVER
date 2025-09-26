900b2b49247a73718d755e405874863a0b306ede
Mopcho <mopchowork@gmail.com>
2025-09-26 10:41:06 +0300

    accept and reveive messages from one connection

--- main.c
+++ main.c
@@ -20,54 +20,65 @@ int main(int argc, char *argv[])
     struct addrinfo *servinfo;
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    int status = getaddrinfo(NULL, argv[1], &hints, &servinfo);
+    int status = getaddrinfo(NULL,argv[1], &hints, &servinfo);
     if (status != 0) {
         fprintf(stderr, "gai error: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    for (struct addrinfo *p = servinfo; p != NULL; p = p->ai_next)
+    for (struct addrinfo* p = servinfo; p != NULL; p = p->ai_next)
     {
-        struct sockaddr_in *ipv4;
-        struct sockaddr_in6 *ipv6;
-        void* addr;
-        char ipstr[INET6_ADDRSTRLEN];
-        unsigned short port;
+        int listen_backlog = 5;
 
-        if (p->ai_family == AF_INET6)
+        int sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1)
         {
-            ipv6 = (struct sockaddr_in6*)p->ai_addr;
-            addr = &ipv6->sin6_addr;
-            port = ntohs(ipv6->sin6_port);
-        } else if (p->ai_family == AF_INET)
-        {
-            ipv4 = (struct sockaddr_in*)p->ai_addr;
-            addr = &ipv4->sin_addr;
-            port = ntohs(ipv4->sin_port);
-        }
-
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-        printf("Host & Port: %s:%hu \n", ipstr, port);
-
-        int socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (socketfd == -1)
-        {
-            fprintf(stderr, "error while acquiring socket; errno: %i \n", errno);
+            printf("Error acquiring socket");
             exit(1);
         }
-        printf("socket file descriptor acquired: %i \n", socketfd);
 
-        int bindres = bind(socketfd, p->ai_addr, p->ai_addrlen);
+        int reuse_socket_opt = 1;
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_socket_opt, sizeof reuse_socket_opt);
+
+        int bindres = bind(sockfd, p->ai_addr, p->ai_addrlen);
         if (bindres == -1)
         {
-            fprintf(stderr, "error while binding socket: %i; errno: %i \n", socketfd, errno);
+            printf("Error binding socket");
             exit(1);
         }
-        printf("successfully bound socketfd: %i \n", socketfd);
+
+        int listenres = listen(sockfd, listen_backlog);
+        if (listenres == -1)
+        {
+            printf("Error trying to listen");
+            exit(1);
+        }
+
+        struct sockaddr_storage incomingcon;
+        socklen_t incomingcon_size = sizeof incomingcon;
+        int newsockfd = accept(sockfd, (struct sockaddr*)&incomingcon, &incomingcon_size);
+        if (newsockfd == -1)
+        {
+            printf("Error accept-ing; errno: %i \n", errno);
+            exit(1);
+        }
+
+        char buf[1024];
+        size_t readbytes;
+        while ((readbytes = recv(newsockfd, buf, sizeof buf, 0)))
+        {
+            printf("%s \n", buf);
+            memset(buf, 0, sizeof buf);
+        }
+        if (readbytes == -1)
+        {
+            printf("Error receiving bytes; errno: %i \n", errno);
+            exit(1);
+        }
     }
 
     freeaddrinfo(servinfo);
